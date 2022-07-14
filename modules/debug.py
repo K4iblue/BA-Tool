332113f949f89a5c6d7a8d11a03cf5c0326a27f0
Kai <kaiklaps@outlook.com>
2022-07-14 13:25:51 +0200

    debug

--- modules/debug.py
+++ modules/debug.py
@@ -2,24 +2,26 @@
 #import os
 #import sys
 #from . import helper_functions as hf
-#from .easyufw import easyufw as ufw
+from .easyufw import easyufw as ufw
 
 def debug(port, protocol='udp', ip=''):
-    print('### DEBUG ###')
-    # Firewall generator (Port Range: 0 - 65536)
-    print('Start Firewall Generator')
+    print('DEBUG: Firewall function call')
+
+    # Start UFW if disabled
+    ufw.enable()
 
     # 1x IP: ufw allow from 203.0.113.103 proto tcp to any port 22
     # Range: ufw allow from 203.0.113.0/24 proto tcp to any port 22
     # Für alle IPs
     if ip == '':
-        print('## DEBUG ## Port ' + str(port) + '/' + str(protocol) + ' wird freigegeben')             # DEBUG
-        #ufw.allow(port, protocol)
+        print('Port ' + str(port) + '/' + str(protocol) + ' wird freigegeben')
+        ufw.allow(port, protocol)
     # Nur bestimmte IPs
     else:
-        print('## DEBUG ## Port ' + str(port) + '/' + str(protocol) + ' wird freigegeben für ' + str(ip))   # DEBUG
-        #ufw.run('allow' + port + '/' + protocol + 'from' + ip)
+        print('Port ' + str(port) + '/' + str(protocol) + ' wird freigegeben für ' + str(ip))
+        ufw.run('allow' + port + '/' + protocol + 'from' + ip)
     
+    ufw.status()
 
 
     #os.system('sudo ufw enable')
@@ -32,6 +34,6 @@ def debug(port, protocol='udp', ip=''):
     #os.system('sudo ufw deny'+port)
     #os.system('sudo ufw deny out'+port)
 
-debug(22,'tcp', '192.168.1.1')
-debug(54,'udp', '192.168.1.2')
-debug(port=80,ip='192.168.1.3')
+#debug(22,'tcp', '192.168.1.1')
+#debug(54,'udp', '192.168.1.2')
+#debug(port=80,ip='192.168.1.3')
