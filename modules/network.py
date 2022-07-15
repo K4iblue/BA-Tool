eb16d85929d451e13f046ad19b93521675e81aa3
Kai <kaiklaps@outlook.com>
2022-07-15 23:54:07 +0200

    debug

--- modules/network.py
+++ modules/network.py
@@ -233,8 +233,8 @@ def ufw_initial_setup():
     # Disable, reset and Enable UFW again
     print('DEBUG: Disable and reset UFW')
     print('DEBUG: Enable UFW')
-    os.system('sudo ufw --force disable')
     subprocess.run('sudo ufw --force reset', shell=True, check=True)
+    os.system('sudo ufw --force disable')
     os.system('sudo ufw --force enable')
 
     # Enable UFW logging with medium severity
