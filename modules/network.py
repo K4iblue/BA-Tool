e509de7661255a7e1cc9cd99e4ca26cccc42e19c
Kai <kaiklaps@outlook.com>
2022-07-16 22:47:27 +0200

    debug

--- modules/network.py
+++ modules/network.py
@@ -329,8 +329,8 @@ def ufw_reset_rules():
     #dict_len = len(all_rules)
     for n in all_rules.items():
         count = 1
-        print(n)
-        #pyufw.delete(str(n))
+        #print(n)
+        pyufw.delete(n)
         print('Debug Count = ' + str(count))
         count += 1
         
