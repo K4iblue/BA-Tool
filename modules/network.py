d27fd1c58491b3d24abd3296e280b127a4958f67
Kai <kaiklaps@outlook.com>
2022-07-16 23:18:39 +0200

    debug

--- modules/network.py
+++ modules/network.py
@@ -235,13 +235,17 @@ def ufw_set_default_settings():
     # Enable UFW
     #print('DEBUG: UFW Enable')
     os.system('sudo ufw enable')
+
     #pyufw.enable()
     
     # Default settings
     print('DEBUG: Deny all incoming and outgoing traffic, set logging to medium')
-    ufw.run('default deny incoming')
-    ufw.run('default deny outgoing')
-    ufw.run('logging medium')
+    #ufw.run('default deny incoming')
+    #ufw.run('default deny outgoing')
+    #ufw.run('logging medium')
+    os.system('sudo ufw default deny incoming')
+    os.system('sudo ufw default deny outgoing')
+    os.system('sudo ufw logging medium')
 
 
 # UFW Rule Generator 
