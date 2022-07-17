2415445676b766f2aedac6209a6319e206230dda
Kai <kaiklaps@outlook.com>
2022-07-17 14:10:34 +0200

    subprocess not working for "cat" and "cp"

--- modules/firewall.py
+++ modules/firewall.py
@@ -99,7 +99,8 @@ def ufw_delete_rules():
 def ufw_allow_ping():
     # Backup before.rules file
     print('DEBUG: Backing up "/etc/ufw/before.rules"')
-    subprocess.run('sudo cp -n /etc/ufw/before.rules{,.bak}', capture_output=True, shell=True, check=True)
+    #subprocess.run('sudo cp -n /etc/ufw/before.rules{,.bak}', capture_output=True, shell=True, check=True)
+    os.system('sudo cp -n /etc/ufw/before.rules{,.bak}')
 
     # Get path to template file
     before_rules_template = os.path.join(sys.path[0]) + '/config/templates/ufw_before_rules.template'
