cafd81d53c5580e6e03e1dea64acbaff7f5d7b72
Kai <kaiklaps@outlook.com>
2022-07-23 21:53:32 +0200

    hide debug menu for screenshot

--- modules/menus.py
+++ modules/menus.py
@@ -17,7 +17,7 @@ def main_menu():
           '3.\t Container Verwaltung \n' +
           '4.\t Systempflege \n' +
           #'5.\t DEBUG \n' +
-          '----- Please enter a number (0-5) -----')
+          '----- Please enter a number (0-4) -----')
 
     # Get a Number from the user in given range
     case_number = hf.get_int(0,6)
