85d0176a1b5f7a15e2d8459e401549a9305de0d7
Kai <kaiklaps@outlook.com>
2022-07-27 22:16:04 +0200

    First start should work now

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-import os
-import sys
 from modules import menus
 from modules import maintenance
 
 def main():
-    # Read from config file
-    config_file = os.path.join(sys.path[0]) + '/config/tool_config.cfg'
-    with open (config_file, 'r', encoding='UTF-8') as file:
-        filedata = file.read()
 
-    if 'first_start=yes' in filedata:
-        print('Erster Programm Start. Es werden nun alle benötigten Packages installiert \n Fortfahren?')
-        print('Fortfahren?')
-
-        start_download = ''
-        while start_download not in ['Y','N']:
-            start_download = input('(y/n): ').upper()
-        start_download = True if start_download == 'Y' else False
-        
-        if start_download is True:
-            maintenance.install_all_needed_packages()
-            # Replace first start line
-            filedata = filedata.replace('first_start=yes', 'first_start=no')
-            # Write to file
-            with open (config_file, 'w+', encoding='UTF-8') as file:
-                file.write(filedata)
-        else:
-            quit()
-        
+    if maintenance.first_start_check() is True:
+        maintenance.first_start_installer()
+        menus.main_menu()
     else:
         menus.main_menu()
 
