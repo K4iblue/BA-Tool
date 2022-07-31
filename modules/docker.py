d73851d09479bcc60f5dbd26e8cc5d3d899b40e9
Kai <kaiklaps@outlook.com>
2022-07-31 15:40:18 +0200

    debug

--- modules/docker.py
+++ modules/docker.py
@@ -245,7 +245,7 @@ def remove_container_firewall_rule(port='', container_ip='', container_name=''):
     all_rules = pyufw.get_rules()
 
     # Get rule index
-    for key in all_rules.items():
+    for key, val in all_rules.items():
         get_values = all_rules.get(key)
         if container_name in get_values:
             rule_index = key
